9cf6b53d0cf55e9163634d567e9d5e8e65e78419
akshay27395 <akshay@cateina.com>
2019-02-24 08:02:19 +0530

    adding Invoices files

---
+++ api/controllers/InvoiceController.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var config = require('../../config/config.json');
+var InvoiceService = require('../services/InvoiceServices');
+
+module.exports = {
+    invoice: invoice,   
+    updateInvoice: updateInvoice,
+    deleteInvoice: deleteInvoice,
+    updateInvoiceStatus: updateInvoiceStatus
+}
+
+
+//  Create Invoice
+function invoice(req, res, next) {
+    var body = req.body;
+      body.date = new Date();
+     // body["invoiceNumber"] = generateId();
+    //   var advPaymentRate = 0.8;
+    //   var advPayment = body.netInvoiceAmount * advPaymentRate;
+    //   var avlLimit = body.availableLimit;
+    //   body.advancePayment = advPayment;
+    //   avlLimit = advPayment - body.financedAmt;
+    //   body.availableLimit = avlLimit;
+    //   body["finanaceAmt"] = avlLimit;
+      InvoiceService.invoice(req,body)
+        .then(function (response) {
+          res.json(response);
+        })
+        .catch(function (response) {
+          var statusCode = response.statusCode;
+          var msg = response.message;
+          res.status(statusCode).send({ message: msg });
+        });
+};
+
+// function generateId() {
+//   var id = config.invoice + Date.now();
+//   return id;
+// }
+
+
+//  Update Invoice
+function updateInvoice(req, res, next) {  
+      var body = req.body;
+      var id = req.swagger.params.id.value;
+    
+      InvoiceService.updateInvoice(req,id, body)
+        .then(function (response) {
+          // utils.writeJson(res, response);
+          res.json(response);
+        })
+        .catch(function (response) {
+          var statusCode = response.statusCode;
+          var msg = response.message;
+          res.status(statusCode).send({ message: msg });
+        });
+  };
+
+  function updateInvoiceStatus(req, res, next) {
+
+    var id = req.swagger.params.id.value;
+    var body = req.body;
+    InvoiceService.updateInvoiceStatus(id, body)
+      .then(function (response) {
+        res.json(response);
+      })
+      .catch(function (response) {
+        var statusCode = response.statusCode;
+        var msg = response.message;
+        res.status(statusCode).send({ message: msg });
+      });
+  };
+
+//  delete invoice data from DB
+function deleteInvoice(req, res, next) {
+
+      var id = req.swagger.params.id.value;
+      var status = req.swagger.params.status.value;
+      InvoiceService.deleteInvoice(id, status)
+        .then(function (response) {
+          res.json(response);
+        })
+        .catch(function (response) {
+          var statusCode = response.statusCode;
+          var msg = response.message;
+          res.status(statusCode).send({ message: msg });
+        });  
+  };
+
+// function getInvoice(req, res, next) {
+ 
+//       var sort = req.swagger.params.sortBy.value || config.sortBy;
+//       var searchBy = req.swagger.params.searchBy.value;
+//       var supplierId = req.swagger.params.supplierId.value;
+//       InvoiceService.getInvoice(sort, searchBy,supplierId)
+//         .then(function (response) {
+//           res.json(response);
+//         })
+//         .catch(function (response) {
+  
+//           res.json(response);
+  
+//         });
+//   };
+
---
+++ api/services/InvoiceServices.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var config = require('../../config/config.json');
+var Q = require('q');
+var collectionName = 'Invoice';
+var crud = require('../utils/CrudServices');
+
+var MODULES = require('../helpers/module');
+const STATUSCODES=MODULES.STATUSCODES();
+
+exports.invoice = function (req, body) {
+
+    var deferred = Q.defer();
+        if (body.status !== STATUSCODES.New) {
+          crud.createData(config.connectionString, config.dbName, collectionName, body)
+            .then(data => {
+              deferred.resolve(data);
+            }).catch(err => { 
+              deferred.reject({ statusCode: 200, message: "Can not create challan" });
+            });
+        } else {
+          condition["status"] = { '$nin': [STATUSCODES.Discarded, STATUSCODES.Draft] };
+
+          crud.insertData(config.connectionString, config.dbName, collectionName, condition, exclude, body)
+            .then(data => {
+              deferred.resolve(data);
+          }).catch(err => {
+              deferred.reject({ statusCode: 200, message: "Invoice with number " + body.invoiceNumber + " already present."});
+          });
+    }
+  return deferred.promise;     
+}
+
+exports.updateInvoice = function(req, id, body) {
+    var deferred = Q.defer();
+    var condition = {};
+      var sendCondition = {};
+      sendCondition['id'] = id;
+    if (body.status !== STATUSCODES.New) {
+      
+      crud.updateData(config.connectionString, config.dbName, collectionName, body, sendCondition, paramNotReq)
+        .then(data => {
+            deferred.resolve(data);
+        }).catch(err => {
+            deferred.reject({ statusCode: 200, message: "Can not create invoice." });
+        });
+    } else {
+
+      body['createDate'] = new Date('' + req.root.date);
+      if (!body.invoiceNumber || body.invoiceNumber === '') {
+        body["invoiceNumber"] = generateId();
+      }
+      condition["status"] = { '$nin': [STATUSCODES.Discarded, STATUSCODES.Draft] };
+      
+      crud.readByCondition(config.connectionString, config.dbName, collectionName, condition, paramNotReq)
+        .then(data => {
+
+            deferred.reject({statusCode : 200 , message:  "Invoice with number " + body.invoiceNumber + " already present."})
+        }) .catch(err => {
+         
+          crud.modifyData(config.connectionString, config.dbName, collectionName, sendCondition,body)
+            
+            .then(data => {
+                deferred.resolve(data);
+            }).catch(err => {    
+                deferred.reject({ statusCode: 200, message: "Can not create invoice" });
+            });
+        });
+    }
+    return deferred.promise;
+}
+
+// Update an existing invoice singal value
+
+exports.updateInvoiceStatus = function (id, body) {
+    var deferred = Q.defer();
+    var condition = {};
+    condition["id"] = id;
+  
+    //  Update (Store data in MongoDB)
+    return crud.updateData(config.connectionString, config.dbName, collectionName, body, condition, paramNotReq);
+  }
